d94231ddded6ba0c4f9a90f22b2c72924c5d97bf
Arron Kennedy <arron_kennedy@hotmail.com>
2018-04-18 14:39:45 +0000

    Added small Jquery exercise completed on Day 9

---
+++ jquery/jquery.js
@@ -0,0 +1,6 @@
+$("div").css("backgroundColor", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "2px solid orange");
+
+firstDiv = $("div")[0]
+$(firstDiv).css("color", "pink");
